7419e569d1f5c7acba4e1a1fe5c85e85a67a750c
cheh <chehovskiy.dima@gmail.com>
2014-09-08 14:44:38 +0300

    Added check for class Cherry_Testimonials not exists

--- cherry-testimonials.php
+++ cherry-testimonials.php
@@ -17,167 +17,171 @@ if ( !defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Sets up and initializes the Cherry Testimonials plugin.
- *
- * @since 1.0.0
- */
-class Cherry_Testimonials {
+// If class 'Cherry_Testimonials' not exists.
+if ( !class_exists( 'Cherry_Testimonials' ) ) {
 
 	/**
-	 * A reference to an instance of this class.
-	 *
-	 * @since 1.0.0
-	 * @var   object
-	 */
-	private static $instance = null;
-
-	/**
-	 * Sets up needed actions/filters for the plugin to initialize.
+	 * Sets up and initializes the Cherry Testimonials plugin.
 	 *
 	 * @since 1.0.0
 	 */
-	public function __construct() {
-
-		// Set the constants needed by the plugin.
-		add_action( 'plugins_loaded', array( $this, 'constants' ), 1 );
-
-		// Internationalize the text strings used.
-		add_action( 'plugins_loaded', array( $this, 'lang' ),      2 );
-
-		// Load the functions files.
-		add_action( 'plugins_loaded', array( $this, 'includes' ),  3 );
-
-		// Load the admin files.
-		add_action( 'plugins_loaded', array( $this, 'admin' ),     4 );
-
-		// Load public-facing style sheet and JavaScript.
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		// add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-
-		// Register activation and deactivation hook.
-		register_activation_hook( __FILE__, array( $this, 'activation'   ) );
-		register_activation_hook( __FILE__, array( $this, 'deactivation' ) );
-	}
-
-	/**
-	 * Defines constants for the plugin.
-	 *
-	 * @since 1.0.0
-	 */
-	function constants() {
+	class Cherry_Testimonials {
 
 		/**
-		 * Set constant name for the post type name.
+		 * A reference to an instance of this class.
+		 *
+		 * @since 1.0.0
+		 * @var   object
+		 */
+		private static $instance = null;
+
+		/**
+		 * Sets up needed actions/filters for the plugin to initialize.
 		 *
 		 * @since 1.0.0
 		 */
-		define( 'CHERRY_TESTI_NAME', 'testimonial' );
+		public function __construct() {
+
+			// Set the constants needed by the plugin.
+			add_action( 'plugins_loaded', array( $this, 'constants' ), 1 );
+
+			// Internationalize the text strings used.
+			add_action( 'plugins_loaded', array( $this, 'lang' ),      2 );
+
+			// Load the functions files.
+			add_action( 'plugins_loaded', array( $this, 'includes' ),  3 );
+
+			// Load the admin files.
+			add_action( 'plugins_loaded', array( $this, 'admin' ),     4 );
+
+			// Load public-facing style sheet and JavaScript.
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+			// add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+			// Register activation and deactivation hook.
+			register_activation_hook( __FILE__, array( $this, 'activation'   ) );
+			register_activation_hook( __FILE__, array( $this, 'deactivation' ) );
+		}
 
 		/**
-		 * Set the version number of the plugin.
+		 * Defines constants for the plugin.
 		 *
 		 * @since 1.0.0
 		 */
-		define( 'CHERRY_TESTI_VERSION', '1.0.0' );
+		function constants() {
+
+			/**
+			 * Set constant name for the post type name.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'CHERRY_TESTI_NAME', 'testimonial' );
+
+			/**
+			 * Set the version number of the plugin.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'CHERRY_TESTI_VERSION', '1.0.0' );
+
+			/**
+			 * Set the name for the 'meta_key' value in the 'wp_postmeta' table.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'CHERRY_TESTI_POSTMETA', '_cherry_testimonial' );
+
+			/**
+			 * Set constant path to the plugin directory.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'CHERRY_TESTI_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+
+			/**
+			 * Set constant path to the plugin URI.
+			 *
+			 * @since 1.0.0
+			 */
+			define( 'CHERRY_TESTI_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+		}
 
 		/**
-		 * Set the name for the 'meta_key' value in the 'wp_postmeta' table.
+		 * Loads files from the '/inc' folder.
 		 *
 		 * @since 1.0.0
 		 */
-		define( 'CHERRY_TESTI_POSTMETA', '_cherry_testimonial' );
+		function includes() {
+			require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-registration.php'  );
+			require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-page-template.php' );
+			require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-data.php' );
+			require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-shortcode.php' );
+			require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-widget.php' );
+		}
 
 		/**
-		 * Set constant path to the plugin directory.
+		 * Loads the translation files.
 		 *
 		 * @since 1.0.0
 		 */
-		define( 'CHERRY_TESTI_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+		function lang() {
+			load_plugin_textdomain( 'cherry-testimonials', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+		}
 
 		/**
-		 * Set constant path to the plugin URI.
+		 * Loads admin files.
 		 *
 		 * @since 1.0.0
 		 */
-		define( 'CHERRY_TESTI_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
-	}
+		function admin() {
 
-	/**
-	 * Loads files from the '/inc' folder.
-	 *
-	 * @since 1.0.0
-	 */
-	function includes() {
-		require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-registration.php'  );
-		require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-page-template.php' );
-		require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-data.php' );
-		require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-shortcode.php' );
-		require_once( trailingslashit( CHERRY_TESTI_DIR ) . 'public/includes/class-cherry-testimonials-widget.php' );
-	}
+			if ( is_admin() ) {
+				require_once( CHERRY_TESTI_DIR . 'admin/includes/class-cherry-testimonials-admin.php' );
+			}
+		}
 
-	/**
-	 * Loads the translation files.
-	 *
-	 * @since 1.0.0
-	 */
-	function lang() {
-		load_plugin_textdomain( 'cherry-testimonials', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-	}
+		/**
+		 * Register and enqueue public-facing style sheet.
+		 *
+		 * @since 1.0.0
+		 */
+		public function enqueue_styles() {
+			wp_enqueue_style( 'cherry-testimonials', plugins_url( 'public/assets/css/style.css', __FILE__ ), array(), CHERRY_TESTI_VERSION );
+		}
 
-	/**
-	 * Loads admin files.
-	 *
-	 * @since 1.0.0
-	 */
-	function admin() {
+		/**
+		 * On plugin activation.
+		 *
+		 * @since 1.0.0
+		 */
+		function activation() {
+			flush_rewrite_rules();
+		}
 
-		if ( is_admin() ) {
-			require_once( CHERRY_TESTI_DIR . 'admin/includes/class-cherry-testimonials-admin.php' );
+		/**
+		 * On plugin deactivation.
+		 *
+		 * @since 1.0.0
+		 */
+		function deactivation() {
+			flush_rewrite_rules();
+		}
+
+		/**
+		 * Returns the instance.
+		 *
+		 * @since  1.0.0
+		 * @return object
+		 */
+		public static function get_instance() {
+
+			// If the single instance hasn't been set, set it now.
+			if ( null == self::$instance )
+				self::$instance = new self;
+
+			return self::$instance;
 		}
 	}
 
-	/**
-	 * Register and enqueue public-facing style sheet.
-	 *
-	 * @since 1.0.0
-	 */
-	public function enqueue_styles() {
-		wp_enqueue_style( 'cherry-testimonials', plugins_url( 'public/assets/css/style.css', __FILE__ ), array(), CHERRY_TESTI_VERSION );
-	}
-
-	/**
-	 * On plugin activation.
-	 *
-	 * @since 1.0.0
-	 */
-	function activation() {
-		flush_rewrite_rules();
-	}
-
-	/**
-	 * On plugin deactivation.
-	 *
-	 * @since 1.0.0
-	 */
-	function deactivation() {
-		flush_rewrite_rules();
-	}
-
-	/**
-	 * Returns the instance.
-	 *
-	 * @since  1.0.0
-	 * @return object
-	 */
-	public static function get_instance() {
-
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance )
-			self::$instance = new self;
-
-		return self::$instance;
-	}
+	Cherry_Testimonials::get_instance();
 }
-
-Cherry_Testimonials::get_instance();
